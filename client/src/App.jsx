0c5841c6c59942c84d1b848458c6c41dfa0aebc8
iamrajatsharma <sharmarajat687@gmail.com>
2024-08-12 15:55:05 +0530

    inital commit

---
+++ client/src/App.jsx
@@ -0,0 +1,71 @@
+import { BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Write from "./pages/Write";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+
+const Layout = () => {
+    return (
+        <>
+            <Navbar />
+            <Outlet />
+            <Footer />
+
+        </>
+    )
+}
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/write",
+                element: <Write />
+            },
+            {
+                path: "/",
+                element: <Home />
+            }
+        ]
+    },
+    {
+        path: "/register",
+        element: <Register />
+    },
+    {
+        path: "/login",
+        element: <Login />
+    }
+])
+
+
+function App() {
+
+    return (
+        <div className="app">
+            <div className="container">
+                <RouterProvider router={router}></RouterProvider>
+                {/* <BrowserRouter>
+                <Routes>
+                    <Route path='/' element="hello">Home</Route>
+                </Routes>
+            </BrowserRouter> */}
+            </div>
+        </div>
+    )
+}
+
+
+
+
+export default App
